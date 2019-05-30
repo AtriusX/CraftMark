8b5d1ef475b1e9037efab475bdefd4a5d4e30156
Atrius <atriusedm@gmail.com>
2019-05-30 03:12:20 -0400

    Test a default renderer implementation

--- src/main/java/io/atrius/ChatListener.java
+++ src/main/java/io/atrius/ChatListener.java
@@ -1,21 +1,22 @@
 package io.atrius;
 
-import io.atrius.render.CraftChatRenderer;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 public class ChatListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         // TODO defer chat messages through commonmark for processing.
         Parser            parser   = Parser.builder().build();
         Node              document = parser.parse(event.getMessage());
-        CraftChatRenderer renderer = new CraftChatRenderer();
+        HtmlRenderer      renderer = HtmlRenderer.builder().build();
 
-        renderer.render(document);
+        event.setMessage(renderer.render(document).trim());
     }
 }
