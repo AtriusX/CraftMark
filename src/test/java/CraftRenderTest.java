17bfb2f5e07d68b34078523a914d03aaf388756d
Atrius <atriusedm@gmail.com>
2019-06-22 21:22:12 -0400

    Fix test class

--- src/test/java/CraftRenderTest.java
+++ src/test/java/CraftRenderTest.java
@@ -1,10 +1,9 @@
-import io.atrius.render.CraftRenderer;
 import org.junit.Test;
 
 public class CraftRenderTest {
 
     @Test
     public void testRenderer() {
-        CraftRenderer renderer = CraftRenderer.create();
+
     }
 }
